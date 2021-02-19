c2c2a4146e67e5c7c69d3eb19cedc65e267733e1
DevsBD <38375448+DevsBD@users.noreply.github.com>
2021-02-19 15:13:28 +0600

    update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === '.ping') {
 
-       message.reply('pong');
+       message.reply('Xcalibr');
 
        } if (true) {
          
